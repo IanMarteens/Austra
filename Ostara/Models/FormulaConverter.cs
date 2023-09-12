4c5232b991e033cc0797a711520c910caedfc0bc
Ian Marteens <ian@marteens.com>
2023-09-12 11:20:14 +0200

    FormulaConverter

---
+++ Ostara/Models/FormulaConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Ostara;
+
+public class FormulaConverter : IValueConverter
+{
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        string formula = (string)value;
+        try
+        {
+            var ans = RootModel.Instance.Environment!.Engine.Eval(formula);
+            if (ans.Type == typeof(double) || ans.Type == typeof(int))
+                return System.Convert.ToDecimal(ans.Value!);
+            return 0M;
+        }
+        catch
+        {
+            if (double.TryParse(formula, out double d))
+                return (decimal)d;
+            return 0M;
+        }
+    }
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        ((decimal)value).ToString(CultureInfo.InvariantCulture);
+}
