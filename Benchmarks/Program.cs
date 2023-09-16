8c1c157dfba28507cb1df6d4d0d72a14e83d1490
Ian Marteens <ian@marteens.com>
2023-09-16 19:33:30 +0200

    Select All command.

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,12 +1,12 @@
 ﻿OutputEncoding = System.Text.Encoding.UTF8;
 WriteLine("Benchmarks for AUSTRA");
 
-//BenchmarkRunner.Run<VectorBenchmark>();
-//BenchmarkRunner.Run<MatrixBenchmark>();
-//BenchmarkRunner.Run<EvdBenchmark>();
-//BenchmarkRunner.Run<LuBenchmark>();
-//BenchmarkRunner.Run<CholeskyBenchmark>();
-//BenchmarkRunner.Run<FftBenchmark>();
-//BenchmarkRunner.Run<SeriesBenchmark>();
-//BenchmarkRunner.Run<ParserBenchmark>();
+BenchmarkRunner.Run<VectorBenchmark>();
+BenchmarkRunner.Run<MatrixBenchmark>();
+BenchmarkRunner.Run<EvdBenchmark>();
+BenchmarkRunner.Run<LuBenchmark>();
+BenchmarkRunner.Run<CholeskyBenchmark>();
+BenchmarkRunner.Run<FftBenchmark>();
+BenchmarkRunner.Run<SeriesBenchmark>();
+BenchmarkRunner.Run<ParserBenchmark>();
 BenchmarkRunner.Run<SplineBenchmark>();
