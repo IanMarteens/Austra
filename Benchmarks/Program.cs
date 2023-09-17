0e237d16f13bd0afc32b63a1290093de63064f8a
Ian Marteens <ian@marteens.com>
2023-09-17 12:38:08 +0200

    Benchmarks.

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,12 +1,6 @@
 ﻿OutputEncoding = System.Text.Encoding.UTF8;
 WriteLine("Benchmarks for AUSTRA");
 
-BenchmarkRunner.Run<VectorBenchmark>();
-BenchmarkRunner.Run<MatrixBenchmark>();
-BenchmarkRunner.Run<EvdBenchmark>();
-BenchmarkRunner.Run<LuBenchmark>();
-BenchmarkRunner.Run<CholeskyBenchmark>();
-BenchmarkRunner.Run<FftBenchmark>();
-BenchmarkRunner.Run<SeriesBenchmark>();
-BenchmarkRunner.Run<ParserBenchmark>();
-BenchmarkRunner.Run<SplineBenchmark>();
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAllJoined();
+
