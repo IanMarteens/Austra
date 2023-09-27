56721d6cc29714ee651764661fe6547f3d567539
Ian Marteens <ian@marteens.com>
2023-09-27 18:46:33 +0200

    Interfaces moved to new file.

--- Austra.Library/Vectors/Matrix.cs
+++ Austra.Library/Vectors/Matrix.cs
@@ -1,25 +1,5 @@
 ﻿namespace Austra.Library;
 
-/// <summary>Common interface for all matrix types.</summary>
-public interface IMatrix
-{
-}
-
-/// <summary>Defines a type with a pointwise multiplication operation.</summary>
-/// <typeparam name="T"></typeparam>
-public interface IPointwiseOperators<T>
-{
-    /// <summary>Item by item multiplication of two data structures.</summary>
-    /// <param name="other">The second operand.</param>
-    /// <returns>A new data structure with all the multiplication results.</returns>
-    T PointwiseMultiply(T other);
-
-    /// <summary>Item by item division of two data structures.</summary>
-    /// <param name="other">The second operand.</param>
-    /// <returns>A new data structure with all the quotient results.</returns>
-    T PointwiseDivide(T other);
-}
-
 /// <summary>Represents a dense rectangular matrix.</summary>
 [JsonConverter(typeof(MatrixJsonConverter))]
 public readonly struct Matrix :
