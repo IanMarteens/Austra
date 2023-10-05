8361041fd665cecf6e2c4cbd985790ee8967987d
Ian Marteens <ian@marteens.com>
2023-10-05 20:56:33 +0200

    ParserBenchmark

--- Benchmarks/ParserBenchmark.cs
+++ Benchmarks/ParserBenchmark.cs
@@ -12,21 +12,21 @@ public class ParserBenchmark : BenchmarkControl
         engine = new AustraEngine(dataSource);
     }
 
-    //[Benchmark]
-    //public Type AustraParseMatrixTrace() =>
-    //    engine.EvalType("let v = vector::nrandom(16) in (v ^ v).trace");
+    [Benchmark]
+    public Type AustraParseMatrixTrace() =>
+        engine.EvalType("let v = vector::nrandom(16) in (v ^ v).trace");
 
-    //[Benchmark]
-    //public Type AustraParseSimpleSum() =>
-    //    engine.EvalType("1.0 + 2.0 * 3.0");
+    [Benchmark]
+    public Type AustraParseSimpleSum() =>
+        engine.EvalType("1.0 + 2.0 * 3.0");
 
-    //[Benchmark]
-    //public double AustraEvalMatrixTrace() =>
-    //    (double)engine.Eval("let v = vector::nrandom(16) in (v ^ v).trace").Value;
+    [Benchmark]
+    public double AustraEvalMatrixTrace() =>
+        (double)engine.Eval("let v = vector::nrandom(16) in (v ^ v).trace").Value;
 
-    //[Benchmark]
-    //public double AustraEvalSimpleSum() =>
-    //    (double)engine.Eval("1.0 + 2.0 * 3.0").Value;
+    [Benchmark]
+    public double AustraEvalSimpleSum() =>
+        (double)engine.Eval("1.0 + 2.0 * 3.0").Value;
 
     [Benchmark]
     public Type AustraParseFunctionCall() =>
