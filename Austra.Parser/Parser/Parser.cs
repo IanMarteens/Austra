0970a437a08e24416caf811650691e4e91151a90
Ian Marteens <ian@marteens.com>
2024-03-04 19:48:57 +0100

    More patterns for optimization.

--- Austra.Parser/Parser/Parser.cs
+++ Austra.Parser/Parser/Parser.cs
@@ -858,7 +858,8 @@ internal sealed partial class Parser : Scanner, IDisposable
                         }
                         else if (e2 is BinaryExpression { NodeType: ExpressionType.Multiply } b2)
                             e1 = OptimizeVectorSum(opAdd, b2, e1, true);
-                        else if (e1 is NewExpression)
+                        else if (e1 is NewExpression 
+                            || e1 is MethodCallExpression { Method.Name: "InplaceAdd" or "InplaceSub"})
                             e1 = opAdd == Token.Plus
                                 ? Expression.Call(e1, typeof(DVector).Get(nameof(DVector.InplaceAdd)), e2)
                                 : Expression.Call(e1, typeof(DVector).Get(nameof(DVector.InplaceSub)), e2);
