04304c3c0599c4477d773de9c49d8c7a2a911cbd
Ian Marteens <ian@marteens.com>
2026-04-16 21:58:57 +0200

    Help

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,15 +1,8 @@
 ﻿OutputEncoding = System.Text.Encoding.UTF8;
 WriteLine("Benchmarks for AUSTRA");
 
-Csv csv = new Csv(@"C:\Users\Marteens\Documents\BmeReports\20151216-UAT\CCURVES-EOD.csv")
-    .WithSeparator(";")
-    .WithFormat("")
-    .WithFilter("Curve Name", "EONIA");
-DVector vector = new(csv, 15);
-Console.WriteLine(vector);
-
 //BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAllJoined();
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAllJoined();
 //BenchmarkRunner.Run<CVectorBenchmark>();
 //BenchmarkRunner.Run<SplineBenchmark>();
 //BenchmarkRunner.Run<FunctionsBenchmark>();
